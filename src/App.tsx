a975b22a7020639a69d46bc0b3436fa1d1fc78cf
Akshay Jain <akshayjain2823@gmail.com>
2025-02-16 21:48:55 +0530

    Initial project setup.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+	return (
+    <>
+      <h1 className="text-6xl font-semibold">Typescript + Tailwind CSS V4.</h1>
+			<Button>Hello</Button>
+		</>
+	);
+}
+
+export default App;
